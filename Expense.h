0867d516e4c1747cabe5386794491a20a5f3582e
Dutu Luiza-Alexandra <luizadutu171@gmail.com>
2026-04-23 00:08:56 +0300

    header pt clasa "expense"

---
+++ Expense.h
@@ -0,0 +1,53 @@
+//
+// Created by Luiza on 22.04.2026.
+//
+
+#ifndef SHARE_THE_BILL_2_0_EXPENSE_H
+#define SHARE_THE_BILL_2_0_EXPENSE_H
+
+#include <iostream>
+#include <string>
+
+
+class Expense {
+
+protected: // ca si "copiii" sa poata foloseasca sau sa modifice atributele
+    std::string description;
+    double total;
+    std::string payerName;
+
+public:
+    Expense();
+    Expense(std::string description, double total, std::string payerName);
+
+    Expense(const Expense& other);
+
+    Expense& operator=(const Expense& other);
+
+   virtual ~Expense(); // este virtual destr la clasa de baza pt a preveni memory leaks
+
+    friend std::ostream& operator<<(std::ostream& out, const Expense& e);
+    friend std::istream& operator>>(std::istream& in, Expense& e);
+
+    std::string getDescription() const;
+    void setDescription(const std::string& newDescription);
+
+    double getTotal() const;
+    void setTotal(double newTotal);
+
+    std::string getPayerName() const;
+    void setPayerName(const std::string& newPayerName);
+
+
+    virtual void calculateShares() const = 0; // metoda pur virtuala si transforma clasa intr-o clasa abstracta
+
+
+    
+
+
+
+};
+
+
+
+#endif //SHARE_THE_BILL_2_0_EXPENSE_H
