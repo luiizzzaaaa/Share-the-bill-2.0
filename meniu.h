e6a4b1f62a5f466e2512b9be098ef775b3d47174
Dutu Luiza-Alexandra <luizadutu171@gmail.com>
2026-04-26 18:17:57 +0300

    Meniul

---
+++ meniu.h
@@ -0,0 +1,38 @@
+//
+// Created by Luiza on 26.04.2026.
+//
+
+#ifndef SHARE_THE_BILL_2_0_MENIU_H
+#define SHARE_THE_BILL_2_0_MENIU_H
+
+#include<iostream>
+#include "Group.h"
+
+class Meniu {
+
+private :
+    Group activeGroup;
+    bool isRunning;
+
+public:
+    Meniu();
+    Meniu(const Group& g);
+    Meniu( const Meniu& m);
+    Meniu& operator=(const Meniu& m);
+    ~Meniu();
+
+
+    friend std::ostream& operator<<(std::ostream& out, const Meniu& m);
+    friend std::istream& operator>>(std::istream& in, Meniu& m);
+
+
+    void run();
+
+
+
+};
+
+
+
+
+#endif //SHARE_THE_BILL_2_0_MENIU_H
